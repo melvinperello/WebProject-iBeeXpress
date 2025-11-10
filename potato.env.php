bfea4a423e6695ef3c48685bac4236220038f9ac
Melvin Perello <melvin.perello@gmail.com>
2025-11-11 05:15:01 +0800

    Added ReadMe Images

--- potato.env.php
+++ potato.env.php
@@ -1,18 +1,18 @@
 <?php
 //database
-define('DB_HOST','localhost',true); // your host
-define('DB_USER','root',true); // your username
-define('DB_PASS','root',true); // your password
-define('DB_NAME','webtwo',true); // your database
+define('DB_HOST','localhost'); // your host
+define('DB_USER','root'); // your username
+define('DB_PASS','root'); // your password
+define('DB_NAME','webtwo'); // your database
 
 //formatting
-define('NL','<br>',true); //Printing new Class for new Line
-define('TAB','&nbsp&nbsp&nbsp&nbsp&nbsp',true); ////Printing new Class for Tab
+define('NL','<br>'); //Printing new Class for new Line
+define('TAB','&nbsp&nbsp&nbsp&nbsp&nbsp'); ////Printing new Class for Tab
 
 //home
-define('HOME','/webmp',true); //Your Project Folder
+define('HOME','/webmp'); //Your Project Folder
 
 //routing
-define('GRANT','main',true); //default route when logged in
-define('DENIED','index',true); //default route when not logged in
+define('GRANT','main'); //default route when logged in
+define('DENIED','index'); //default route when not logged in
 ?>
